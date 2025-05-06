42f21f1953a082ddc2f7e826691b0ab572357403
TYLY24 <>
2025-05-06 18:09:45 +0700

    bai9+10+11

---
+++ HelloWorld/assets/DAY1/bai11/AddItem.js
@@ -0,0 +1,25 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+     onLoad () 
+     {
+        this.itemCatalog = ["Sword", "Shield", "Potion"]
+     },
+
+    onClickAddItem () {
+        this.itemCatalog.push("new Item");
+    },
+    onClickedShow()
+    {
+        console.log(this.itemCatalog);
+    }
+
+    // update (dt) {},
+});
